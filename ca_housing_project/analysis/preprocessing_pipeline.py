21336ff554aebf93bef5bba59fac342dabc86c0a
Samuel Humberto Fandino Bogoya <fandinos@io.maas>
2025-09-18 20:00:44 -0400

    Add all model notebooks and preprocessing pipeline

---
+++ ca_housing_project/analysis/preprocessing_pipeline.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+import os
+
+PROJECT_ROOT_DIR = ".."
+train_path = os.path.join(PROJECT_ROOT_DIR, "data", "train", "housing_train.csv")
+housing = pd.read_csv(train_path)
+
+# Custom transformer
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+housing_num = housing.drop("ocean_proximity", axis=1)
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+])
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", OneHotEncoder(), cat_attribs),
+])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+processed_path = os.path.join(PROJECT_ROOT_DIR, "data", "train", "housing_train_processed.csv")
+pd.DataFrame(housing_prepared).to_csv(processed_path, index=False)
